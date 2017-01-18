e564d7225530d84863221ffaffd76d22514a0303
Oleksandr <avpavlov96@gmail.com>
2017-01-18 18:11:29 +0300

    Initial commit

---
+++ src/main/java/ua/alexandroid1/alex/CustomerRepository.java
@@ -0,0 +1,9 @@
+package ua.alexandroid1.alex;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+	List<Customer> findByLastNameStartsWithIgnoreCase(String lastName);
+}
